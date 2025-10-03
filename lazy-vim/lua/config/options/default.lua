b62a25eb605c5cbe05db2b4da2c12624ca811a07
clang-engineer <clang.engineer@gmail.com>
2025-10-03 16:59:34 +0900

    feat: nvim dianostics config

--- lazy-vim/lua/config/options/default.lua
+++ lazy-vim/lua/config/options/default.lua
@@ -3,3 +3,15 @@ vim.opt.relativenumber = false
 
 -- set conceallevel
 vim.opt.conceallevel = 0
+
+-- set dianostic virtual text
+vim.diagnostic.enable(false)
+-- vim.diagnostic.config({
+--   severity_sort = true,
+--   virtual_text = {
+--     severity = vim.diagnostic.severity.ERROR,
+--   },
+--   signs = {
+--     severity = { min = vim.diagnostic.severity.WARN },
+--   },
+-- })
