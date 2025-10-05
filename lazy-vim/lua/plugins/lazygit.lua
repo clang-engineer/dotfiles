63b93ca58db0e99b48bec07dcbc7bc01f27e4c39
clang-engineer <clang.engineer@gmail.com>
2025-10-05 13:25:17 +0900

    chore: commen

--- lazy-vim/lua/plugins/lazygit.lua
+++ lazy-vim/lua/plugins/lazygit.lua
@@ -1,4 +1,7 @@
--- lazygit.nvim: git UI를 nvim에서 사용
+-- lazygit.nvim: git UI를 nvim에서 사용,
+-- 별도 사전 설치 필요,
+-- mac: brew install lazygit
+-- ubuntu: apt install lazygit
 -- nvim v0.8.0
 return {
   "kdheepak/lazygit.nvim",
