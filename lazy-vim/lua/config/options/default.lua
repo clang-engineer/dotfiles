05a4d66fb175f07bc7ac8a859f29a77c0d57ca33
clang-engineer <clang.engineer@gmail.com>
2025-10-14 08:51:07 +0900

    chore: diagnostic config

--- lazy-vim/lua/config/options/default.lua
+++ lazy-vim/lua/config/options/default.lua
@@ -6,12 +6,10 @@ vim.opt.conceallevel = 0
 
 -- set dianostic virtual text
 -- vim.diagnostic.enable(false)
--- vim.diagnostic.config({
---   severity_sort = true,
---   virtual_text = {
---     severity = vim.diagnostic.severity.ERROR,
---   },
---   signs = {
---     severity = { min = vim.diagnostic.severity.WARN },
---   },
--- })
+vim.diagnostic.config({
+  severity_sort = true,
+  virtual_text = { severity = vim.diagnostic.severity.ERROR },
+  signs = { severity = { max = vim.diagnostic.severity.ERROR } },
+  underline = { severity = vim.diagnostic.severity.ERROR },
+  float = { severity = vim.diagnostic.severity.ERROR },
+})
