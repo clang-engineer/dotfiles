a914305b50d519de8fe37f9c75f26326e37a9702
clang-engineer <clang.engineer@gmail.com>
2026-02-01 10:17:25 +0900

    fix(diagnostics): disable diagnostics in markdown buffers

--- nvim/lazy/lua/config/options/default.lua
+++ nvim/lazy/lua/config/options/default.lua
@@ -13,3 +13,10 @@ vim.diagnostic.config({
   underline = { severity = vim.diagnostic.severity.ERROR },
   float = { severity = vim.diagnostic.severity.ERROR },
 })
+
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = { "markdown" },
+  callback = function()
+    vim.diagnostic.enable(false, { bufnr = 0 })
+  end,
+})
