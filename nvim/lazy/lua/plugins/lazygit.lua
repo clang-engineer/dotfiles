32de610100a26d79458951a7bd35cad4a8328413
clang.engineer <clang.engineer@gmail.com>
2026-03-13 10:28:57 +0900

    fix(lazygit): disable snacks.nvim lazygit editPreset on Windows
    On Windows, snacks.nvim's lazygit integration fails due to the use of
    bash
    
    syntax (e.g., '[') in the "nvim-remote" editPreset, which causes errors
    
    when running under cmd.exe. This change disables the editPreset and sets
    
    editCommand to "nvim" with a compatible template, ensuring proper
    
    functionality on Windows systems.

--- nvim/lazy/lua/plugins/lazygit.lua
+++ nvim/lazy/lua/plugins/lazygit.lua
@@ -9,4 +9,24 @@ return {
   dependencies = {
     "nvim-lua/plenary.nvim",
   },
+
+  -- snacks.nvim lazygit: Windows에서 editPreset "nvim-remote"가
+  -- bash 문법([)을 사용하여 cmd.exe에서 에러 발생 → 비활성화
+  {
+    "folke/snacks.nvim",
+    opts = {
+      lazygit = {
+        configure = true,
+        config = {
+          os = {
+            editPreset = "",
+            editCommand = "nvim",
+            editCommandTemplate = '{{editor}} -- "{{filename}}"',
+            shell = "bash",
+            shellArg = "-c",
+          },
+        },
+      },
+    },
+  },
 }
