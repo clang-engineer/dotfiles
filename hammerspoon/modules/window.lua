1c498294cadf5d5279fadfd2488c21eddd5f73e0
clang-engineer <clang.engineer@gmail.com>
2026-03-24 09:37:43 +0900

    feat(hammerspoon): update window hotkeys and modifiers

--- hammerspoon/modules/window.lua
+++ hammerspoon/modules/window.lua
@@ -1,7 +1,7 @@
 -- show winow hint
 hs.hotkey.bind({'shift'}, 'F1', hs.hints.windowHints)
 
--- move window 
+-- move window
 local function move_win(xx, yy, ww, hh)
     return function()
         local win = hs.window.focusedWindow()
@@ -15,18 +15,15 @@ local function move_win(xx, yy, ww, hh)
     end
 end
 
-local mod = {'option', 'shift'}
-hs.hotkey.bind(mod, 'left', move_win(0, 0, 1/2, 1))
-hs.hotkey.bind(mod, 'right', move_win(1/2, 0, 1/2, 1))
-hs.hotkey.bind(mod, 'up', move_win(0, 0, 1, 1/2))
-hs.hotkey.bind(mod, 'down', move_win(0, 1/2, 1, 1/2))
+local mod = {'ctrl', 'option'}
 
-hs.hotkey.bind(mod, '1', move_win(0, 1/2, 1/2, 1/2))
-hs.hotkey.bind(mod, '2', move_win(0, 1/2, 1, 1/2))
-hs.hotkey.bind(mod, '3', move_win(1/2, 1/2, 1/2, 1/2))
-hs.hotkey.bind(mod, '4', move_win(0, 0, 1/2, 1))
-hs.hotkey.bind(mod, '5', move_win(0, 0, 1, 1))
-hs.hotkey.bind(mod, '6', move_win(1/2, 0, 1/2, 1))
-hs.hotkey.bind(mod, '7', move_win(0, 0, 1/2, 1/2))
-hs.hotkey.bind(mod, '8', move_win(0, 0, 1, 1/2))
-hs.hotkey.bind(mod, '9', move_win(1/2, 0, 1/2, 1/2))
+-- Rectangle style keybindings
+hs.hotkey.bind(mod, 'left', move_win(0, 0, 1/2, 1))       -- 왼쪽 반
+hs.hotkey.bind(mod, 'right', move_win(1/2, 0, 1/2, 1))    -- 오른쪽 반
+hs.hotkey.bind(mod, 'up', move_win(0, 0, 1, 1/2))          -- 위쪽 반
+hs.hotkey.bind(mod, 'down', move_win(0, 1/2, 1, 1/2))      -- 아래쪽 반
+hs.hotkey.bind(mod, 'return', move_win(0, 0, 1, 1))         -- 전체화면
+hs.hotkey.bind(mod, 'u', move_win(0, 0, 1/2, 1/2))          -- 왼쪽 상단
+hs.hotkey.bind(mod, 'j', move_win(0, 1/2, 1/2, 1/2))        -- 왼쪽 하단
+hs.hotkey.bind(mod, 'i', move_win(1/2, 0, 1/2, 1/2))        -- 오른쪽 상단
+hs.hotkey.bind(mod, 'k', move_win(1/2, 1/2, 1/2, 1/2))      -- 오른쪽 하단
