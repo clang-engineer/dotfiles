ade31456f89972cf153704f81b7b515b1cfec059
clang-engineer <clang.engineer@gmail.com>
2026-03-25 08:44:11 +0900

    feat(hammerspoon): enhance window management hotkeys

--- hammerspoon/modules/window.lua
+++ hammerspoon/modules/window.lua
@@ -1,4 +1,4 @@
--- show winow hint
+-- show window hint
 hs.hotkey.bind({'shift'}, 'F1', hs.hints.windowHints)
 
 -- move window
@@ -7,8 +7,8 @@ local function move_win(xx, yy, ww, hh)
         local win = hs.window.focusedWindow()
         local frame = win:frame()
         local screen = win:screen():frame()
-        frame.x = screen.w * xx
-        frame.y = screen.h * yy
+        frame.x = screen.x + screen.w * xx
+        frame.y = screen.y + screen.h * yy
         frame.w = screen.w * ww
         frame.h = screen.h * hh
         win:setFrame(frame)
@@ -17,13 +17,27 @@ end
 
 local mod = {'ctrl', 'option'}
 
--- Rectangle style keybindings
-hs.hotkey.bind(mod, 'left', move_win(0, 0, 1/2, 1))       -- 왼쪽 반
-hs.hotkey.bind(mod, 'right', move_win(1/2, 0, 1/2, 1))    -- 오른쪽 반
-hs.hotkey.bind(mod, 'up', move_win(0, 0, 1, 1/2))          -- 위쪽 반
-hs.hotkey.bind(mod, 'down', move_win(0, 1/2, 1, 1/2))      -- 아래쪽 반
-hs.hotkey.bind(mod, 'return', move_win(0, 0, 1, 1))         -- 전체화면
-hs.hotkey.bind(mod, 'u', move_win(0, 0, 1/2, 1/2))          -- 왼쪽 상단
-hs.hotkey.bind(mod, 'j', move_win(0, 1/2, 1/2, 1/2))        -- 왼쪽 하단
-hs.hotkey.bind(mod, 'i', move_win(1/2, 0, 1/2, 1/2))        -- 오른쪽 상단
-hs.hotkey.bind(mod, 'k', move_win(1/2, 1/2, 1/2, 1/2))      -- 오른쪽 하단
+-- Rectangle style keybindings: 반분할
+hs.hotkey.bind(mod, 'left', move_win(0, 0, 1/2, 1))        -- Left Half
+hs.hotkey.bind(mod, 'right', move_win(1/2, 0, 1/2, 1))     -- Right Half
+hs.hotkey.bind(mod, 'up', move_win(0, 0, 1, 1/2))           -- Top Half
+hs.hotkey.bind(mod, 'down', move_win(0, 1/2, 1, 1/2))       -- Bottom Half
+
+-- 꼭짓점
+hs.hotkey.bind(mod, 'u', move_win(0, 0, 1/2, 1/2))          -- Top Left
+hs.hotkey.bind(mod, 'i', move_win(1/2, 0, 1/2, 1/2))        -- Top Right
+hs.hotkey.bind(mod, 'j', move_win(0, 1/2, 1/2, 1/2))        -- Bottom Left
+hs.hotkey.bind(mod, 'k', move_win(1/2, 1/2, 1/2, 1/2))      -- Bottom Right
+
+-- 전체화면 / 중앙
+hs.hotkey.bind(mod, 'return', move_win(0, 0, 1, 1))          -- Maximize
+hs.hotkey.bind(mod, 'c', move_win(1/8, 1/8, 3/4, 3/4))      -- Center (3/4 크기)
+
+-- 1/3 분할
+hs.hotkey.bind(mod, 'd', move_win(0, 0, 1/3, 1))            -- First Third
+hs.hotkey.bind(mod, 'f', move_win(1/3, 0, 1/3, 1))          -- Center Third
+hs.hotkey.bind(mod, 'g', move_win(2/3, 0, 1/3, 1))          -- Last Third
+
+-- 2/3 분할
+hs.hotkey.bind(mod, 'e', move_win(0, 0, 2/3, 1))            -- First Two Thirds
+hs.hotkey.bind(mod, 't', move_win(1/3, 0, 2/3, 1))          -- Last Two Thirds
