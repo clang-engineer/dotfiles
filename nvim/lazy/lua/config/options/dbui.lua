7efc072aca35c646659092bc0c878d3efc56c8a6
clang.engineer <clang.engineer@gmail.com>
2026-05-06 13:40:13 +0900

    docs(dbui): improve query usage docs and result window UX

--- nvim/lazy/lua/config/options/dbui.lua
+++ nvim/lazy/lua/config/options/dbui.lua
@@ -1,6 +1,6 @@
 vim.g.dbs = {
   {
-    name = "docker postgres",
+    name = "Docker postgres",
     url = "postgres://myuser@localhost:5432/mydb",
   },
   {
@@ -8,3 +8,11 @@ vim.g.dbs = {
     url = "postgresql://myuser@REDACTED_IP:5432/mydb?connect_timeout=5",
   },
 }
+
+-- 쿼리 결과창(dbout)을 화면 절반 높이로 키운다
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "dbout",
+  callback = function()
+    vim.cmd("resize " .. math.floor(vim.o.lines / 2))
+  end,
+})
