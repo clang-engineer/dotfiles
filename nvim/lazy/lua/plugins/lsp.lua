4cd24f7c26023d961000998ad0985bd5c33a282a
clang-engineer <clang.engineer@gmail.com>
2026-06-16 13:15:42 +0900

    fix(nvim): disable kotlin LSP documentHighlight (fwcd #600 workaround)
    fwcd/kotlin-language-server v1.3.13 throws NoTopLevelDescriptorProvider on
    documentHighlight requests over class/data class/enum braces, surfacing as
    recurring `-32603: Internal error` notifications on CursorHold. Fix exists on
    main (PR #612) but was not included in the v1.3.13 release tag. Tracking
    release request at fwcd/kotlin-language-server#671.

--- nvim/lazy/lua/plugins/lsp.lua
+++ nvim/lazy/lua/plugins/lsp.lua
@@ -1,12 +1,18 @@
 -- lsp.lua: LSP 관련 설정 모음
 return {
   -- clangd: C/C++ 언어 서버 UTF-16 인코딩 설정
+  -- kotlin_language_server: fwcd v1.3.13 #600 우회 (documentHighlight 끔). v1.3.14 릴리스되면 제거 (fwcd/kotlin-language-server#671)
   {
     "neovim/nvim-lspconfig",
     opts = {
       setup = {
         clangd = function(_, opts)
           opts.capabilities.offsetEncoding = { "utf-16" }
+        end,
+        kotlin_language_server = function(_, opts)
+          opts.on_attach = function(client, _)
+            client.server_capabilities.documentHighlightProvider = false
+          end
         end,
       },
     },
