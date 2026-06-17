01796e6845eea2a526f37a6d682a6177ca7ee27c
clang-engineer <clang.engineer@gmail.com>
2026-06-17 23:12:58 +0900

    refactor(nvim): use external jvm-env plugin

--- nvim/lazy/init.lua
+++ nvim/lazy/init.lua
@@ -1,8 +1,5 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
 
--- Setup Java environment (before jdtls starts)
-require("jvm-env").setup()
-
 -- toolbox 빠른 뷰어 (:Toolbox / :ToolboxGrep)
 require("toolbox").setup()
