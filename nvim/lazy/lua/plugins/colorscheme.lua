e5852baf4285c2e2b7373abe442973cecbf8a149
clang-engineer <clang.engineer@gmail.com>
2026-07-12 20:22:51 +0900

    feat(nvim): add more colorscheme plugins

--- nvim/lazy/lua/plugins/colorscheme.lua
+++ nvim/lazy/lua/plugins/colorscheme.lua
@@ -5,10 +5,23 @@ return {
     "LazyVim/LazyVim",
     opts = { colorscheme = "tokyonight" },
   },
-  {
-    "ellisonleao/gruvbox.nvim",
-    lazy = true,
-  },
+  { "ellisonleao/gruvbox.nvim", lazy = true },
+  -- 요즘 평판 오르는 신흥 (:colorscheme 으로 비교용)
+  { "vague-theme/vague.nvim", lazy = true }, -- 저대비 뮤트 파스텔
+  { "scottmckendry/cyberdream.nvim", lazy = true }, -- 비비드 네온 사이버펑크
+  { "nyoom-engineering/oxocarbon.nvim", lazy = true }, -- IBM Carbon 미니멀 모노톤
+  { "comfysage/evergarden", lazy = true }, -- 저채도 그린
+  { "ribru17/bamboo.nvim", lazy = true }, -- 그린 계열
+  { "craftzdog/solarized-osaka.nvim", lazy = true }, -- solarized 다크 변주
+  -- de-facto 상위권 (신흥과 비교 기준선)
+  { "rose-pine/neovim", name = "rose-pine", lazy = true }, -- 뮤트 파스텔, Soho vibes
+  { "rebelot/kanagawa.nvim", lazy = true }, -- 우키요에 다크
+  { "sainnhe/everforest", lazy = true }, -- 저채도 포레스트 그린
+  { "EdenEast/nightfox.nvim", lazy = true }, -- 팔레트 세트
+  { "navarasu/onedark.nvim", lazy = true }, -- Atom OneDark
+  { "sainnhe/gruvbox-material", lazy = true }, -- gruvbox 저대비 개선판
+  { "shaunsingh/nord.nvim", lazy = true }, -- 북유럽 블루
+  { "Mofiqul/dracula.nvim", lazy = true }, -- 보라 대비
   {
     "catppuccin/nvim",
     lazy = true,
