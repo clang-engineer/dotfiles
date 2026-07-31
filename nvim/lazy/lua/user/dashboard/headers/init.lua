851b92551c944c5c6e7d27604aa5b8016838effb
clang-engineer <clang.engineer@gmail.com>
2026-07-31 09:38:41 +0900

    feat: cat lua

--- nvim/lazy/lua/user/dashboard/headers/init.lua
+++ nvim/lazy/lua/user/dashboard/headers/init.lua
@@ -1,4 +1,6 @@
+local render_cats = require("user.dashboard.renderers.cats")
+
 return {
-  require("user.dashboard.headers.duo-cats"),
-  require("user.dashboard.headers.magic-cat"),
+  { text = require("user.dashboard.headers.duo-cats"), render = render_cats },
+  { text = require("user.dashboard.headers.magic-cat"), render = render_cats },
 }
