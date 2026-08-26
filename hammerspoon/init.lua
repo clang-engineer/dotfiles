7fc1f1e7f5b8b79375e6c1951f770c3e13b39fea
clang <clang.engineer@gmail.com>
2026-08-26 14:19:39 +0900

    feat: load window keymap menu

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -3,3 +3,4 @@ hs.ipc.cliInstall() -- enable `hs` CLI for remote reload/eval
 require('modules.rectangle')
 require('modules.window-hint')
 require('modules.aerospace-windows')
+require('modules.keymap-help')
