19c71e7fa65dd66073e167ec84d7f7cb2635c3d0
clang <clang.engineer@gmail.com>
2026-08-26 14:08:17 +0900

    chore: remove stale hjkl module reference

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -1,6 +1,5 @@
 hs.ipc.cliInstall() -- enable `hs` CLI for remote reload/eval
 
---require('modules.hjkl')
 require('modules.rectangle')
 require('modules.window-hint')
 require('modules.aerospace-windows')
