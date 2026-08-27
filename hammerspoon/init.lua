947bef47e355f793ef18b54b976599d6f05c898a
clang <clang.engineer@gmail.com>
2026-08-27 11:20:43 +0900

    refactor(hammerspoon): remove redundant window hint module

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -1,6 +1,5 @@
 hs.ipc.cliInstall() -- enable `hs` CLI for remote reload/eval
 
 require('modules.rectangle')
-require('modules.window-hint')
 require('modules.aerospace-windows')
 require('modules.keymap-help')
